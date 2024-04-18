39d901d3b8026548f292de63d9c23360790851f4
Alex W <alex@blinkenlights.ca>
2024-04-18 10:20:35 -0400

    First version of the one-click deploy demo

---
+++ docker_scripts/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+# shellcheck source=runtime/functions
+source "${APP_HOME}/functions"
+
+[[ ${DEBUG} == true ]] && set -x
+
+# default behaviour is to launch postgres
+if [[ -z ${1} ]]; then
+  run_frontend
+  run_backend
+
+else
+  exec "$@"
+fi
